018961d432b57f8511d29f7bfae5616d9180ef79
Hugo Izquierdo <izquierdoh88@gmail.com>
2023-03-20 15:00:26 -0700

    adding app.js changes

--- app.js
+++ app.js
@@ -1,48 +1,58 @@
 //be able to interact with our application
 const api = require('./api.js');
-const app = require('./app.js');
 
 const prompts = require('prompts');
 
 
 
-const searchPrompt = async (category) => {
-    /* const displayCategories = api.getCategory(category).map(cat => {
+/* const searchPrompt = async (category) => {
+    const displayCategories = api.getCategory(category).map(cat => {
         return {strMeal: `${cat.strMeal}`, strMealThumb: `${cat.strMealThumb}`, id: `${cat.idMeal}`};
-    }); */
+    }); 
     const displayCategories = await api.getCategory(category);
+    //const displayRecipe = await api.getCategory(category).mealId;
+    //console.log(displayRecipe);
     const mealType = displayCategories;
+    
 
     return await prompts([
         {
             type: 'multiselect',
             name: 'Recipe Categories',
             message: 'Select the recipe category to explore',
-            choices: mealType,
-            validate: (selected) => {
-                const maxSelection = 1;
-                if (selected.length > maxSelection){
-                    return 'You may only select 1 recipe' ;
-                } else {
-                    return true;
-                }
-            }
+            choices: mealType
+        }
+        //onsubmit(getRecipe(prompts, getRecipe(mealType)));
+    ]);
+}; */
 
+const searchPrompt = async (results) => {
+    const displayCategories = results.map((result) => {
+        return {title: `${result.strMeal}`, value: result.idMeal};
+    }); 
+    return await prompts([
+        {
+            type: 'select',
+            name: 'Recipe Categories',
+            message: 'Select the specific recipe to explore',
+            choices: displayCategories
         }
     ]);
 };
-//www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
 
-/* const searchRecipeId = async (category) => {
-    const buildCat = await api.getCategory(category);
-    const getRecId = await buildCat.idMeal;
-    console.log(getRecId);
-    
-}; */
+const search = async (args) => {
+    //uses the user selection to retrieve meal
+    const res = await api.getCategory(args);
+    //console.log(res);
+    const selected = await searchPrompt(res.meals);
+    //console.log(res.meals);
+    const recipeFull = await api.getRecipe(selected['Recipe Categories']);
+
+};
 
 module.exports = {
     //same as getRecipe : getRecipe
-    searchPrompt
+    search
 };
 
-//console.log(searchPrompt('Beef'));
+//console.log(search('Beef and Mustard Pie'));
